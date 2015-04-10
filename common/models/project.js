c9bdc6e3aae55455123939e73f7cb2ded1e736e3
Peter Ordonez <peter.ordonez@gmail.com>
2015-04-10 19:05:51 -0400

    working

--- common/models/project.js
+++ common/models/project.js
@@ -1,38 +1,30 @@
-var loopback  = require('../../node_modules/loopback/index');
-var Role = loopback.Role;
-var RoleMapping = loopback.RoleMapping;
 var debug = require('../../node_modules/loopback/node_modules/debug')('aclmod:Project');
 var util = require('util');
 var assert = require('assert');
 
 module.exports = function(Project) {
 
-  Project.afterRemote('create', function(context, project) {
+
+  Project.afterRemote('create', function(context, project, next) {
+    var Team = Project.app.models.Team;
+    var AccessManager = Project.app.models.AccessManager;
     assert(context.req.accessToken, 'Projects may only be created by authenticated users');
 
     var accessToken = context.req.accessToken;
-    debug("project: " + util.inspect(project) + ", accessToken: " + util.inspect(accessToken));
+    debug('afterRemote(create) Creating project: ' + util.inspect(project) + ', accessToken: ' + util.inspect(accessToken));
 
-    var userId = accessToken.userId
-    var rolePrefix = userId + "_" +project.name + "_";
+    // Add the user to the team
+    Team.create({projectId: project.id, userId: accessToken.userId}, function(err, team) {
+      debug('afterRemote(create) Creating team: ' + util.inspect(team) + ', err: ' + util.inspect(err));
 
-    Role.create({name: rolePrefix+'_admin'}, function(err, role) {
-      debug("role: " + util.inspect(role) + ", err: " + util.inspect(err));
-      if (err) throw err;
-      role.principals.create({principalType: RoleMapping.USER, principalId: userId}, function(err, p) {
-        debug("role mapping: " + util.inspect(p) + ", err: " + util.inspect(err));
-        if (err) throw err;
+      AccessManager.manageInstance(AccessManager.ADMIN,
+                                   'Project',
+                                   project.id,
+                                   accessToken.userId, 
+                                   function(err, am) {
+        debug('afterRemote(create) Managing instance. am: ' + util.inspect(am) + ', err: ' + util.inspect(err), ', calling next()');
+        next();
       });
-    });
-
-    Role.create({name: rolePrefix+'_editor'}, function(err, role) {
-      debug("role: " + util.inspect(role) + ", err: " + util.inspect(err));
-      if (err) throw err;
-    });
-
-    Role.create({name: rolePrefix+'_viewer'}, function(err, role) {
-      debug("role: " + util.inspect(role) + ", err: " + util.inspect(err));
-      if (err) throw err;
     });
   });
 };
